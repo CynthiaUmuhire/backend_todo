da48aeafbc3da538a3c8c05c1cdafd180125f113
CynthiaUmuhire <c.umuhire@alustudent.com>
2023-12-11 11:33:50 +0200

    Define RestAPI endpoints for the tasks

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,4 +1,24 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Post } from '@nestjs/common';
 
 @Controller('tasks')
-export class TasksController {}
+export class TasksController {
+  @Get()
+  findAll(): string {
+    return 'This action returns all tasks';
+  }
+
+  @Get(':id')
+  findOne(id: string) {
+    return `This action returns a #${id} task`;
+  }
+
+  @Post()
+  create(@Body() body: any) {
+    return 'This action adds a new task = ' + body;
+  }
+
+  @Delete(':id')
+  remove(id: string) {
+    return `This action removes a #${id} task`;
+  }
+}
