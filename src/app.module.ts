1307071830a2e57b83da3cb124e050df7f6b4bc5
CynthiaUmuhire <c.umuhire@alustudent.com>
2023-12-11 11:27:10 +0200

    feat: add TasksController and CategoriesController

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TasksModule } from './tasks/tasks.module';
+import { CategoriesModule } from './categories/categories.module';
+import { TasksController } from './tasks/tasks.controller';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [TasksModule, CategoriesModule],
+  controllers: [AppController, TasksController],
   providers: [AppService],
 })
 export class AppModule {}
